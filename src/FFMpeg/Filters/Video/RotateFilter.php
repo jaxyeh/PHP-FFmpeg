3c9c79b962b8f8712699f9b6d206211fd46a09bb
Jason Yeh <jason@streamoso.com>
2013-12-03 22:16:38 -0500

    Auto-detect Rotate Filter

--- src/FFMpeg/Filters/Video/RotateFilter.php
+++ src/FFMpeg/Filters/Video/RotateFilter.php
@@ -19,6 +19,7 @@ use FFMpeg\Format\VideoInterface;
 
 class RotateFilter implements VideoFilterInterface
 {
+    const ROTATE_AUTO = 'auto';
     const ROTATE_90 = 'transpose=1';
     const ROTATE_180 = 'hflip,vflip';
     const ROTATE_270 = 'transpose=2';
@@ -28,7 +29,7 @@ class RotateFilter implements VideoFilterInterface
     /** @var integer */
     private $priority;
 
-    public function __construct($angle, $priority = 0)
+    public function __construct($angle = self::ROTATE_AUTO, $priority = 0)
     {
         $this->setAngle($angle);
         $this->priority = (int) $priority;
@@ -55,6 +56,34 @@ class RotateFilter implements VideoFilterInterface
      */
     public function apply(Video $video, VideoInterface $format)
     {
+        foreach ($video->getStreams()->videos() as $stream) {
+            if ($this->angle == self::ROTATE_AUTO) {
+                $metadata = $stream->get('tags');
+                if (!empty($metadata)) {
+                    // Auto Detect Rotation
+                    if(array_key_exists('rotate', $metadata)) {
+                        $rotation = $metadata['rotate'];
+                        // k, so the "rotate" Metadata existed!
+                        if ($rotation==90) {
+                            $this->angle = self::ROTATE_90;
+                        } elseif($rotation==180) {
+                            $this->angle = self::ROTATE_180;
+                        } elseif ($rotation==270 || $rotation==-90) {
+                            $this->angle = self::ROTATE_270;
+                        }
+                    }
+                }
+            }
+
+            if (in_array($this->angle, array(self::ROTATE_90, self::ROTATE_270), true)) {
+                if ($stream->has('width') && $stream->has('height')) {
+                    $width = $stream->get('width');
+                    $stream->set('width', $stream->get('height'));
+                    $stream->set('height', $width);
+                }
+            }
+        }
+/*
         if (in_array($this->angle, array(self::ROTATE_90, self::ROTATE_270), true)) {
             foreach ($video->getStreams()->videos() as $stream) {
                 if ($stream->has('width') && $stream->has('height')) {
@@ -64,13 +93,17 @@ class RotateFilter implements VideoFilterInterface
                 }
             }
         }
-
-        return array('-vf', $this->angle, '-metadata:s:v:0', 'rotate=0');
+*/
+        if ($this->angle !== self::ROTATE_AUTO )
+            return array('-vf', $this->angle, '-metadata:s:v:0', 'rotate=0');
+        else
+            return array();
     }
 
     private function setAngle($angle)
     {
         switch ($angle) {
+            case self::ROTATE_AUTO:
             case self::ROTATE_90:
             case self::ROTATE_180:
             case self::ROTATE_270:
